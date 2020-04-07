d55ebc4d4e8ed09be8bef13eb05441807ef6171a
adel995 <adbatal95@gmail.com>
2020-04-08 01:08:57 +0300

    deleted error file

--- src/containers/App.js
+++ src/containers/App.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
 import Scroll from '../components/Scroll';
-import ErrorBoundry from '../components/ErrorBoundry';
 import './App.css';
 
 
